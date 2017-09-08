9a7998df973ce0db69eb416f581ccf1722fa3b9a
Joca <jpereyraleon@gmail.com>
2017-09-08 00:02:24 -0500

    get nearest stations

--- app/Http/Controllers/StationsController.php
+++ app/Http/Controllers/StationsController.php
@@ -4,11 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Station;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StationsController extends Controller
 {
     public function index()
     {
         return Station::all();
+    }
+
+    public function findNear()
+    {
+        $lat = request('lat');
+        $long = request('long');
+
+        return DB::table('stations AS s')
+                 ->select(DB::Raw("* ,
+                    (
+                       6371 *
+                       acos(cos(radians($lat)) *
+                       cos(radians(s.lat)) *
+                       cos(radians(s.long) -
+                       radians($long)) +
+                       sin(radians($lat)) *
+                       sin(radians(s.lat)))
+                    ) AS distance"))
+                ->orderBy('distance', 'asc')
+                ->take(5)
+                ->get();
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,4 @@
 Route::post('/incidents', 'IncidentsController@store');
 Route::get('/incidents', 'IncidentsController@index');
 Route::get('/stations', 'StationsController@index');
+Route::post('/stations/near', 'StationsController@findNear');
