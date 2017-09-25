16ff69980a26f3c14076c7a77f0b2bd2d66e0106
Joca <jpereyraleon@gmail.com>
2017-09-25 00:50:01 -0500

    add turf

--- resources/assets/js/components/StationMap.vue
+++ resources/assets/js/components/StationMap.vue
@@ -11,37 +11,83 @@
         :clickable="true"
         :draggable="false"
       ></gmap-marker>
+        <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :draggable="false"
+        @click="center=m.position"
+      ></gmap-marker>
     </gmap-map>
+    <div>
+      <button class="btn btn-default" v-on:click="calculateHotSpots()">Identificar puntos de conflicto</button>
+      <button class="btn btn-default" v-on:click="generateRoute()">Generar Ruta</button>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import { loaded } from 'vue2-google-maps';
+import clustersKmeans from '@turf/clusters-kmeans';
+import {featureCollection, point} from '@turf/turf';
 
 export default {
   props: ['center_lat', 'center_lng', 'id'],
+  data () {
+    return {
+      markers: [],
+    }
+  },
   async mounted () {
     await loaded;
+    let vm = this;
     const incidents = await axios.get('/api/incidents');
     const station = await axios.get(`/api/stations/${this.id}`);
     const geoJson = JSON.parse(station.data.jurisdictions.geojson);
     const json = this.$refs.map.$mapObject.data.addGeoJson(geoJson);
     const jurisdiction = new google.maps.Polygon({paths: json[0].getGeometry().getArray()})
 
-    let points = _.filter(incidents.data, function(incindent) {
+    vm.points = _.filter(incidents.data, function(incindent) {
       const location = new google.maps.LatLng(incindent.lat, incindent.long);
       return google.maps.geometry.poly.containsLocation(location, jurisdiction)
     });
 
-    points = _.map(points, function(incindent) {
+    let points = _.map(vm.points, function(incindent) {
       return new google.maps.LatLng(incindent.lat, incindent.long);
     });
-    this.heatmap = new google.maps.visualization.HeatmapLayer({
-      data: points,
-    })
 
-    this.heatmap.setMap(this.$refs.map.$mapObject);
+    vm.heatmap = new google.maps.visualization.HeatmapLayer({
+      data: points,
+    });
+    vm.heatmap.setMap(this.$refs.map.$mapObject);
+  },
+  methods: {
+    calculateHotSpots () {
+      let vm = this;
+      let points = _.map(this.points, function(incindent) {
+        return point([incindent.lat, incindent.long]);
+      });
+      let hotspots = [];
+      let clusters = clustersKmeans(featureCollection(points), 8);
+      _.each(clusters.features, (c) => {
+        hotspots[c.properties.cluster] = c.properties.centroid;
+      });
+      _.each(hotspots, function(hotspot) {
+        vm.markers.push({
+          position: {lat: hotspot[0], lng: hotspot[1]}
+        });
+      });
+    },
+    generateRoute () {
+      console.log('a');
+    }
   }
+
 }
 </script>
+<style>
+  .btn {
+    margin-top: 10px;
+  }
+</style>
